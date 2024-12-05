8f61fce0a5482f4f47b9acdcd58d019648f38859
Babazade <babazadevasif14@gmail.com>
2024-12-05 12:23:40 +0400

    anottther

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import ViteHtmlPlugin from 'vite-plugin-html';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteHtmlPlugin()],
   optimizeDeps: {
     include: ['swiper'],
   },
